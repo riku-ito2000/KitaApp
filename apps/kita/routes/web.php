dc857f2d9f7a11b9b4f5bbb0f936f6c7200458d8
riku-ito2000 <r_ito@craid-inc.com>
2024-07-30 13:21:05 +0900

    fixed register form

--- apps/kita/routes/web.php
+++ apps/kita/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,6 +19,18 @@ Route::get('/', function () {
     return view('home');
 });
 
-Auth::routes();
+// 既存の登録ルートをコメントアウト
+// Auth::routes();
+
+// 新しい登録ルートを定義
+Route::middleware('guest')->group(function () {
+    // 会員登録ルート
+    Route::get('member_registration', [RegisterController::class, 'showRegistrationForm'])->name('register');
+    Route::post('member_registration', [RegisterController::class, 'register']);
+
+    // ログインルート
+    Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
+    Route::post('login', [LoginController::class, 'login']);
+});
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
