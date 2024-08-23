818fd624ae1bf3f5f139becf9adbb3e9d0240bcd
riku-ito2000 <r_ito@craid-inc.com>
2024-08-23 16:35:04 +0900

    phpstan

--- apps/kita/app/Http/Controllers/ProfileController.php
+++ apps/kita/app/Http/Controllers/ProfileController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
 
 class ProfileController extends Controller
 {
