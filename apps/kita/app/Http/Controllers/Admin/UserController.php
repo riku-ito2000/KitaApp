d947669d58a376df37890f8d6d06691ae5cf6b44
riku-ito2000 <r_ito@craid-inc.com>
2024-09-05 16:34:06 +0900

    add the escape

--- apps/kita/app/Http/Controllers/Admin/UserController.php
+++ apps/kita/app/Http/Controllers/Admin/UserController.php
@@ -9,15 +9,17 @@ use Illuminate\Http\Request;
 class UserController extends Controller
 {
     /**
+     * Displaying the admin member list and searching the admins
+     *
      * @param Request $request
      * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
      */
     public function index(Request $request)
     {
         // クエリパラメータを取得
-        $last_name = $request->input('last_name');
-        $first_name = $request->input('first_name');
-        $email = $request->input('email');
+        $last_name = $this->escapeLike($request->input('last_name'));
+        $first_name = $this->escapeLike($request->input('first_name'));
+        $email = $this->escapeLike($request->input('email'));
 
         // ページネーションの設定を取得
         $paginationLimit = config('pagination.admin_users', 10);
@@ -45,5 +47,20 @@ class UserController extends Controller
         $message = $admin_users->isEmpty() ? '管理者が見つかりませんでした。' : null;
 
         return view('admin.user.index', compact('admin_users', 'message'));
+    }
+
+    /**
+     * LIKEクエリ用に特殊文字をエスケープ
+     *
+     * @param string|null $value
+     * @return string
+     */
+    private function escapeLike(?string $value): string
+    {
+        if ($value === null) {
+            return '';
+        }
+
+        return str_replace(['\\', '%', '_'], ['\\\\', '\\%', '\\_'], $value);
     }
 }
