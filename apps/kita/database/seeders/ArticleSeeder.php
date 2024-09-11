c85182259d7c1e788d4c0d62b8b3d15dda215f27
riku-ito2000 <r_ito@craid-inc.com>
2024-09-11 16:35:50 +0900

    added tags seeder& modified index

--- apps/kita/database/seeders/ArticleSeeder.php
+++ apps/kita/database/seeders/ArticleSeeder.php
@@ -15,19 +15,26 @@ class ArticleSeeder extends Seeder
         $faker = Faker::create();
         // タグを全て取得
         $tags = ArticleTag::all();
+        // メンバーを全て取得
+        $members = Member::all();
 
         // 100件のデータを追加
         for ($i = 1; $i <= 100; $i++) {
-            $article = Article::create([
-                'title' => $faker->sentence,
-                'contents' => $faker->paragraph,
-                'member_id' => $faker->numberBetween(1, 100), // 1から100までのランダムな member_id を指定
-            ]);
+            // メンバーが存在する場合にランダムにメンバーを選択
+            if ($members->isNotEmpty()) {
+                $randomMember = $members->random()->id;
 
-            // タグが存在する場合にのみランダムにタグを付与
-            if ($tags->isNotEmpty()) {
-                $randomTags = $tags->random(rand(1, 3))->pluck('id')->toArray();
-                $article->tags()->attach($randomTags);
+                $article = Article::create([
+                    'title' => $faker->sentence,
+                    'contents' => $faker->paragraph,
+                    'member_id' => $randomMember, // ランダムな member_id を指定
+                ]);
+
+                // タグが存在する場合にのみランダムにタグを付与
+                if ($tags->isNotEmpty()) {
+                    $randomTags = $tags->random(rand(1, 20))->pluck('id')->toArray();
+                    $article->tags()->attach($randomTags);
+                }
             }
         }
     }
