2453554f6c0e4820fb157dbb019855633ce224d3
riku-ito2000 <r_ito@craid-inc.com>
2024-09-13 15:58:41 +0900

    updated edit page

--- apps/kita/resources/views/admin/tag/edit.blade.php
+++ apps/kita/resources/views/admin/tag/edit.blade.php
@@ -1,1 +1,12 @@
-edit page
+@extends('layouts.app')
+
+@section('title', '管理者編集 - Kita')
+
+@section('container')
+
+    <div class="container">
+        <h2 class="mb-4">タグ管理 - 編集</h2>
+        <!-- フラッシュメッセージ -->
+    @include('common.messages')
+    </div>
+@endsection
